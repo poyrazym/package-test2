080770cf9b9f24ea241537b43bac9d44063c991f
ymp <ymp.dev1@gmail.com>
2023-06-21 12:07:21 +0300

    derived

--- src/ClassLibrary2/Class2.cs
+++ src/ClassLibrary2/Class2.cs
@@ -1,6 +1,8 @@
-﻿namespace ClassLibrary2
+﻿using ClassLibrary1;
+
+namespace ClassLibrary2
 {
-    public class Class1
+    public class Class2 : Class1
     {
         public string? DerivedName { get; set; }
         public DateTime DerivedTime { get; set; }
